4264d08c5c7903ed47501cdd80dc3b6f2b9853e1
soycoder <43911669+soycoder@users.noreply.github.com>
2020-04-22 15:40:31 +0700

    upload assets

---
+++ assets/js/auth.js
@@ -0,0 +1,34 @@
+function authentication() {
+//   e.preventDefault();
+  loading();
+  var data = {
+    username: "",
+    password: "",
+  };
+
+  data["username"] = document.getElementById("InputStudentID").value;
+  data["password"] = document.getElementById("InputPassword1").value;
+
+  axios
+    .post("https://auth-tu-api.herokuapp.com/api/auth", data)
+    .then(function (res) {
+      console.log(res.data);
+      if(res.data.status === true){
+        localStorage.type = res.data.type;
+        localStorage.displayname_th = res.data.displayname_th;
+        localStorage.displayname_en = res.data.displayname_en;
+        localStorage.faculty = res.data.faculty;
+        location.replace("/information.html")
+      }
+    })
+    .catch(function (err) {
+      console.log(err.message);
+    });
+}
+
+function loading() {
+  document.getElementById(
+    "login"
+  ).innerHTML = `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>
+  <span class="sr-only">Loading...</span>`;
+}
